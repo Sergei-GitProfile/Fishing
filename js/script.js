f8ce143d9259f0ad37d16f6f5474dc0bb97c0b7d
Сергей Гольцев <adresss183@gmail.com>
2020-04-29 16:59:33 +0300

    commit №4

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 const $=window.jQuery;
 $(function(){
 
-
     $("#spool .item__link-product").click(function () {
-
-        $(".catalogue1__menu__closed").toggleClass("catalogue__menu__open");
+        $(".catalogue1__menu__closed").toggleClass("catalogue1__menu__open");
         $(this).closest(".product-group__item").toggleClass("product-group__item_blue");
         $(this).toggleClass("item__link-product_blue");
     });
 
    $("#spool .link-product_button").click(function () {
-
-        $(".catalogue1__menu__closed").toggleClass("catalogue__menu__open");
+        $(".catalogue1__menu__closed").toggleClass("catalogue1__menu__open");
         $(this).closest(".product-group__item").toggleClass("product-group__item_blue");
         $(this).prev(".item__link-product").toggleClass("item__link-product_blue");
     });
+
+    $(".nav__item_catalogue").click(function () {
+        $(".catalogue__menu").toggleClass("catalogue__menu_opened");
+    });
+
 });
